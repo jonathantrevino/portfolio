61d5e5dcd9dcb9b6961290af2cbe3ba2aa58b4ea
jonathantrevino <yeahimjt+github@gmail.com>
2025-01-23 23:15:48 -0600

    dynamically rendering project cards

--- components/project/card.tsx
+++ components/project/card.tsx
@@ -4,19 +4,20 @@ import Image from 'next/image'
 import Skill from '../skill';
 import Link from 'next/link';
 
-const skills = ['Next.js', 'TypeScript', 'Tailwind', 'Firebase', 'AWS'];
 
-const Card = () => {
+const Card = ({ badge, image, title, description, skills, slug, live_link }: { badge: boolean, image: string, title: string, description: string, skills: string[], slug: string, live_link: string }) => {
   return (
     <div className='border border-card-stroke px-8 py-6 rounded-lg space-y-5'>
       <div className='space-y-8'>
-        <Badge title={'MOST POPULAR'} />
+        {badge &&
+          <Badge title={'MOST POPULAR'} />
+        }
         <div className='space-y-2'>
           <span className='flex items-center gap-2'>
-            <Image src='/icons/webportfolios.svg' width={28} height={28} alt='webportfolios.dev icon' />
-            <h5>webportfolios.dev</h5>
+            <Image src={image} width={28} height={28} alt={`${title} icon`} />
+            <h5>{title}</h5>
           </span>
-          <p className='text-text-body leading-160'>A directory of developer portfolios, browse for inspiration or upload your own to start building your online presence.</p>
+          <p className='text-text-body leading-160'>{description}</p>
           <div className='flex flex-wrap gap-2'>{
             skills.map((skill, index) =>
               <Skill key={index} title={skill} />
@@ -25,8 +26,8 @@ const Card = () => {
         </div>
       </div>
       <div className='flex gap-4 items-center'>
-        <button className='bg-custom-blue-button text-custom-blue-button-text p-2 rounded-[4px]'>Read More</button>
-        <Link href='https://webportfolios.dev?ref=jonathantrevino.com' target='_blank' rel='noreferrer'>
+        <Link href={slug} className='bg-custom-blue-button text-custom-blue-button-text p-2 rounded-[4px]'>Read More</Link>
+        <Link href={live_link} target='_blank' rel='noreferrer'>
           <Image src='/icons/external.svg' width={24} height={24} alt='exteral icon' />
         </Link>
       </div>
--- sections/project.tsx
+++ sections/project.tsx
@@ -2,11 +2,29 @@ import Card from '@/components/project/card'
 import Image from 'next/image'
 import React from 'react'
 
+const projects = [
+  {
+    badge: true,
+    image: '/icons/webportfolios.svg',
+    title: 'webportfolios.dev',
+    description: 'A directory of developer portfolios, browse for inspiration or upload your own to start building your online presence.',
+    skills: ['Next.js', 'TypeScript', 'Tailwind', 'Firebase', 'AWS'],
+    slug: '/projects/webportfolios.dev',
+    live_link: 'https://webportfolios.dev?ref=jonathantrevino.com'
+
+  }
+]
+
+
+
 const Project = () => {
   return (
     <section className='mx-15 px-6 space-y-6'>
       <div className='flex items-center gap-4'><Image src='/icons/folder.svg' width={57} height={50} alt='folder icon' /><h2 className='pt-3'>Projects</h2></div>
-      <Card />
+      {projects.map((project, index) =>
+        <Card key={index} badge={project.badge} image={project.image} title={project.title} description={project.description} skills={project.skills} slug={project.slug} live_link={project.live_link} />
+
+      )}
     </section>
   )
 }
